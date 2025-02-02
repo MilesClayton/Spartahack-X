91511f1e1561ccae7429b0ba1ea1d2f28d3d31a4
Jacob Patton <patton33@msu.edu>
2025-02-02 10:21:59 -0500

    Added threat image to email

--- emailbackup.py
+++ emailbackup.py
@@ -1,7 +1,10 @@
 import smtplib
+from email import encoders
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import base64
+import os
 
 
 # Function to send an email
@@ -20,6 +23,10 @@ def send_email(to_email, subject, name, data):
     message["To"] = to_email
     message["Subject"] = subject
 
+    if data["image_data"] is not None:
+        with open(f"decoded_{data['image_name']}", "wb") as image_file:
+            image_file.write(base64.b64decode(data["image_data"]))
+    print("Image decoded and saved!")
 
     # Generic string with variables
     body = f"""
@@ -39,6 +46,16 @@ def send_email(to_email, subject, name, data):
     """
 
     message.attach(MIMEText(body, "plain"))
+
+    # Attach image
+    image_path = f"decoded_{data['image_name']}"
+    if os.path.exists(image_path):
+        with open(image_path, "rb") as attachment:
+            mime_base = MIMEBase("application", "octet-stream")
+            mime_base.set_payload(attachment.read())
+            encoders.encode_base64(mime_base)
+            mime_base.add_header("Content-Disposition", f"attachment; filename={os.path.basename(image_path)}")
+            message.attach(mime_base)
 
     try:
         # Connect to the SMTP server and send the email
